ca7e57e44603acbca87f7e1d7fe80e94252870b1
SonTrungTo <son.trung.to@gmail.com>
2022-05-15 19:45:49 +0300

    dict done!

---
+++ dict.py
@@ -0,0 +1,25 @@
+# key: value data structure
+# ordered, changeable but unduplicable
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+print(thisdict.get("brand"))
+print(thisdict.keys())
+print(thisdict.values())
+print(thisdict.items())
+print("salad" in thisdict)
+
+# dict.update({key: value})
+
+# dict.pop()
+# dict.popItem()
+# del
+# dict.clear()
+
+# for x, y in thisdict.items():
+#  print(x, y)
+
