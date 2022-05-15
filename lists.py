98b04b2170822574d474659a325f537940eb291a
SonTrungTo <son.trung.to@gmail.com>
2022-05-15 17:26:49 +0300

    lists done!

---
+++ lists.py
@@ -0,0 +1,7 @@
+## New concepts: List comprehension
+## Syntax: newlist = [expression for item in iterable if condition == True]
+## NOTE: This is different from JS!
+
+## Sort: List.sort(reverse: bool)
+##       List.sort(key: Func)
+
