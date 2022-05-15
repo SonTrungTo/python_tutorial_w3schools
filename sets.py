01a7e40459107f0bc6c428bf92299dc1f6782664
SonTrungTo <son.trung.to@gmail.com>
2022-05-15 19:25:29 +0300

    sets done!

---
+++ sets.py
@@ -0,0 +1,19 @@
+# unordered, unchangeable, unindexed and unduplicable
+thisset = { "apple", "banana", "cherry", "apple" }
+print(thisset)
+
+# accessing element by looping
+
+# set.add(element)
+# set.update(set2)
+# set.remove(element): raise error if not exists
+# set.discard(element): not raise error if not exists
+# set.pop()
+# set.clear()
+# del set
+# set.union(set2) => new set
+# set.intersection_update(set2)
+# set.intersection(set2) => new set
+# set.symmetric_difference_update(set2)
+# set.symmetric_difference(set2) => new set
+
