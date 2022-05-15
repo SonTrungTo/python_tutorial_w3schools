f5e8005da738fb8268093a96c08da407736af5c9
SonTrungTo <son.trung.to@gmail.com>
2022-05-15 11:52:59 +0300

    variables done!

---
+++ variables.py
@@ -0,0 +1,14 @@
+x, y, z = "Orange", "Vader", "Palpy"
+x = y = z = "Fish"
+print(x)
+print(y)
+print(z)
+print(x + y + z)
+
+fruits = [1,2,3]
+x, y, z = fruits
+print(x,y,z)
+print(x + y + z)
+
+## also global vs local variable. Put global
+## inside a func if you want to change a variable.
